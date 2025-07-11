1372daa7df1949385b4f22eb10c3bd33b89d86f5
AmeyR1202 <ameyrakhe01@gmail.com>
2025-07-11 16:48:49 +0530

    Designed the hero page

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid-background">
+        <HeroSection />
+      </div>
+    </div>
+  );
+}
---
+++ components/Hero.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">
+            Your AI Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+            Get ahead with customized career support, cutting-edge AI tools, and
+            proven strategies to land your dream role.
+          </p>
+        </div>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+
+          <Link href="https://www.youtube.com/">
+            <Button size="lg" className="px-8" variant="outline">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
